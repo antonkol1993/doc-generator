42a05111c5e56f73c333fa6ac4378af090eda1fd
Anton office <antonkol1993@gmail.com>
2025-05-07 17:47:06 +0300

    Модуль repo 1. Заполнил начальные данные тестовые в новый объект

--- repo/src/main/java/com/antonio/persistence/config/DbInit.java
+++ repo/src/main/java/com/antonio/persistence/config/DbInit.java
@@ -1,52 +1,45 @@
 package com.antonio.persistence.config;
 
-import com.antonio.persistence.entity.TestProduct;
-import com.antonio.persistence.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Value;
+import com.antonio.persistence.entity.ObjectToGenerator;
+import com.antonio.persistence.repository.ObjectToGeneratorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.util.List;
 
 @Component
 public class DbInit implements CommandLineRunner {
 
-    private final ProductRepository productRepository;
+    private final ObjectToGeneratorRepository objectRepository;
 
-    public DbInit(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public DbInit(ObjectToGeneratorRepository objectRepository) {
+        this.objectRepository = objectRepository;
     }
-
-    @Value("${app.database.path}")
-    private String dbPath;
-
 
     @Override
     public void run(String... args) throws Exception {
+        List<ObjectToGenerator> objects = List.of(
+                create("Шуруп универсальный", "Universal Screw", "ШурупУниверсальный", "/images/screw1.jpg"),
+                create("Саморез по металлу", "Metal Self-tapping Screw", "СаморезМеталл", "/images/screw2.jpg"),
+                create("Гайка М8", "Nut M8", "ГайкаМ8", "/images/nut1.jpg"),
+                create("Шайба гроверная", "Spring Washer", "ШайбаГровер", "/images/washer1.jpg"),
+                create("Шуруп для дерева", "Wood Screw", "ШурупДерево", "/images/screw3.jpg"),
+                create("Болт с шестигранной головкой", "Hex Head Bolt", "БолтШестигр", "/images/bolt1.jpg")
+        );
 
-//        // Добавляем пример продукта в базу данных
-//        TestProduct testProduct = new TestProduct();
-//        testProduct.setName("ANTON12131");
-//        testProduct.setPrice(99.99);
-//
-//        // Сохраняем его в базу данных
-//        productRepository.save(testProduct);
-//
-//        // Добавляем пример продукта в базу данных
-//        TestProduct testProduct2 = new TestProduct();
-//        testProduct2.setName("ANTON2");
-//        testProduct2.setPrice(213231321.0);
-//
-//        // Сохраняем его в базу данных
-//        productRepository.save(testProduct2);
-//
-//        // Выводим список всех продуктов
-//        productRepository.findAll().forEach(p ->
-//                System.out.println(p.getName()
-//                ));
+        objectRepository.saveAll(objects);
 
+        objectRepository.findAll().forEach(o ->
+                System.out.println(o.getOriginalName() + " | " + o.getRusName())
+        );
+    }
+
+    private ObjectToGenerator create(String rusName, String originalName, String to1C8, String imagePath) {
+        ObjectToGenerator obj = new ObjectToGenerator();
+        obj.setOriginalName(originalName);
+        obj.setRusName(rusName);
+        obj.setTo1C8name(to1C8);
+        obj.setImagePath(imagePath);
+        return obj;
     }
 }
--- repo/src/main/java/com/antonio/persistence/entity/ObjectToGenerator.java
+++ repo/src/main/java/com/antonio/persistence/entity/ObjectToGenerator.java
@@ -2,28 +2,24 @@ package com.antonio.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-
 
 @Data
 @Entity(name = "object_main")
 public class ObjectToGenerator {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Value("orig_name")
+    @Column(name = "orig_name")
     private String originalName;
 
-    @Value("rus_name")
+    @Column(name = "rus_name")
     private String rusName;
 
-    @Value("1C8_name")
+    @Column(name = "1C8_name")
     private String To1C8name;
 
-    @Value("imagePath")
+    @Column(name = "image_path")
     private String imagePath;
-
-
-
 }
